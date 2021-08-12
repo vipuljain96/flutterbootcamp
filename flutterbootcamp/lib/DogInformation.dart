0392631692d335781a3798084e6a7c55c0508509
Vipul <vjvipul96@gmail.com>
2021-08-12 16:42:27 +0530

    Connection with Database

--- flutterbootcamp/lib/DogInformation.dart
+++ flutterbootcamp/lib/DogInformation.dart
@@ -1,189 +1,83 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-//
-// import 'database.dart';
-//
-// class DogInformation extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() {
-//     return _DogInformationState();
-//   }
-// }
-//
-// class _DogInformationState extends State<DogInformation> {
-//
-//   @override
-//   void initState() {
-//     SystemChrome.setEnabledSystemUIOverlays([]);
-//     super.initState();
-//   }
-//   final myname = TextEditingController();
-//   final myage = TextEditingController();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         child: ListView(
-//           children: <Widget>[
-//             Container(
-//               width: MediaQuery.of(context).size.width,
-//               height: MediaQuery.of(context).size.height/3.5,
-//               decoration: BoxDecoration(
-//                   gradient: LinearGradient(
-//                     begin: Alignment.topCenter,
-//                     end: Alignment.bottomCenter,
-//                     colors: [
-//                       Color(0xff6bceff),
-//                       Color(0xff6bceff)
-//                     ],
-//                   ),
-//                   borderRadius: BorderRadius.only(
-//                       bottomLeft: Radius.circular(90)
-//                   )
-//               ),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: <Widget>[
-//                   Spacer(),
-//                   Align(
-//                     alignment: Alignment.center,
-//                     child: Icon(Icons.folder,
-//                       size: 90,
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                   Spacer(),
-//
-//                   Align(
-//                     alignment: Alignment.bottomRight,
-//                     child: Padding(
-//                       padding: const EdgeInsets.only(
-//                           bottom: 32,
-//                           right: 32
-//                       ),
-//                       child: Text('Information About Dog',
-//                         style: TextStyle(
-//                             color: Colors.white,
-//                             fontSize: 18
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//
-//             Container(
-//               height: MediaQuery.of(context).size.height/2,
-//               width: MediaQuery.of(context).size.width,
-//               padding: EdgeInsets.only(top: 62),
-//               child: Column(
-//                 children: <Widget>[
-//                   Container(
-//                     width: MediaQuery.of(context).size.width/1.2,
-//                     height: 45,
-//                     padding: EdgeInsets.only(
-//                         top: 4,left: 16, right: 16, bottom: 4
-//                     ),
-//                     decoration: BoxDecoration(
-//                         borderRadius: BorderRadius.all(
-//                             Radius.circular(50)
-//                         ),
-//                         color: Colors.white,
-//                         boxShadow: [
-//                           BoxShadow(
-//                               color: Colors.black12,
-//                               blurRadius: 5
-//                           )
-//                         ]
-//                     ),
-//                     child: TextField(
-//                       controller: myname,
-//                       decoration: InputDecoration(
-//                         border: InputBorder.none,
-//                         hintText: 'Name of Dog',
-//
-//                       ),
-//                     ),
-//                   ),
-//
-//                   Container(
-//                     width: MediaQuery.of(context).size.width/1.2,
-//                     height: 45,
-//                     margin: EdgeInsets.only(top: 32),
-//                     padding: EdgeInsets.only(
-//                         top: 4,left: 16, right: 16, bottom: 4
-//                     ),
-//                     decoration: BoxDecoration(
-//                         borderRadius: BorderRadius.all(
-//                             Radius.circular(50)
-//                         ),
-//                         color: Colors.white,
-//                         boxShadow: [
-//                           BoxShadow(
-//                               color: Colors.black12,
-//                               blurRadius: 5
-//                           )
-//                         ]
-//                     ),
-//                     child: TextField(
-//                       controller: myage,
-//                      // obscureText: true,
-//                       decoration: InputDecoration(
-//                         border: InputBorder.none,
-//                         hintText: 'Age',
-//                       ),
-//                     ),
-//                   ),
-//
-//                   Spacer(),
-//
-//                   InkWell(
-//                     onTap: (){
-//                       //var database =new databasehelper();
-//                       var data = Dog(
-//                         id: 0,
-//                         name: myname.text,
-//                         age: int.parse(myage.text),
-//                       );
-//                       // dogdata(data);
-//
-//                     },
-//                     child: Container(
-//                       height: 45,
-//                       width: MediaQuery.of(context).size.width/1.2,
-//                       decoration: BoxDecoration(
-//                           gradient: LinearGradient(
-//                             colors: [
-//                               Color(0xff6bceff),
-//                               Color(0xFF00abff),
-//                             ],
-//                           ),
-//                           borderRadius: BorderRadius.all(
-//                               Radius.circular(50)
-//                           )
-//                       ),
-//                       child: Center(
-//                         child: Text('Submit'.toUpperCase(),
-//                           style: TextStyle(
-//                               color: Colors.white,
-//                               fontWeight: FontWeight.bold
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             SizedBox(
-//               height: 50,
-//             ),
-//           ],
-//
-//         ),
-//       ),
-//     );
-//   }
-//
-// }
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'database.dart';
+var dogDao = new DogDao();
+var database;
+void main() async{
+  database = dogDao.openDb();
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  //pair programming -- driver, navigator
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'add dog',
+      home: Scaffold(
+        appBar: AppBar(title: Text('insert dog'),),
+        body: AddDogForm(),
+      ),
+    );
+  }
+}
+
+class AddDogForm extends StatefulWidget {
+  const AddDogForm({Key? key}) : super(key: key);
+
+  @override
+  _AddDogFormState createState() => _AddDogFormState();
+}
+
+
+class _AddDogFormState extends State<AddDogForm> {
+  int id = 0;
+  int age = 0;
+  String name = '';
+  var idController = TextEditingController();
+  var nameController = TextEditingController();
+  var ageController = TextEditingController();
+
+  void _addDog() async{
+    setState(() {
+      id = int.parse(idController.text);
+      age = int.parse(ageController.text);
+      name = nameController.text;
+    });
+    var dog = Dog(id:id,name:name,age: age);
+    await dogDao.insertDog(dog);
+  }
+  void _getDogs(){
+    dogDao.getDogs();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: idController,
+              decoration: InputDecoration(labelText: 'id'),
+            ),
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'name'),
+            ),
+            TextFormField(
+              controller: ageController,
+              decoration: InputDecoration(labelText: 'age'),
+            ),
+            RaisedButton(onPressed: _addDog, child: Text('add dog'),),
+            RaisedButton(onPressed: _getDogs, child: Text('get dog'),),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- flutterbootcamp/lib/database.dart
+++ flutterbootcamp/lib/database.dart
@@ -1,89 +1,69 @@
 import 'dart:async';
-import 'package:flutter/widgets.dart';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:flutter/widgets.dart';
+
+class DogDao {
+  var database;
+
+  DogDao(){
+    print('dog dao constructor called');
+    openDb();
+  }
 
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
-  final database = openDatabase(
-    join(await getDatabasesPath(), 'doggie_database.db'),
-    onCreate: (db, version) {
-      return db.execute(
-        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
-      );
-    },
-    version: 1,
-  );
+  Future<Database> openDb() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    database = openDatabase(join(await getDatabasesPath(), 'dog.db'),
+        version: 1,
+        onCreate: (db, version) {
+          String sql =
+              'CREATE TABLE dogs(id INTEGER PRIMARY KEY,name TEXT,age INTEGER)';
+          db.execute(sql);
+          print('table created');
+        });
+    if(database != null) {
+      print('db is not null');
+    }
+    return database;
+  }
 
   Future<void> insertDog(Dog dog) async {
     final db = await database;
-
-    await db.insert(
-      'dogs',
-      dog.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
+    int position = await db.insert('dogs', dog.toMap(),
+        nullColumnHack: null, conflictAlgorithm: ConflictAlgorithm.replace);
+    print('inserted dog--'+dog.name);
+    print("inserted at position ="+ position.toString());
   }
 
-  Future<List<Dog>> dogs() async {
+  Future<List<Dog>> getDogs() async {
     final db = await database;
-
-    final List<Map<String, dynamic>> maps = await db.query('dogs');
-
-    return List.generate(maps.length, (i) {
-      return Dog(
-        id: maps[i]['id'],
-        name: maps[i]['name'],
-        age: maps[i]['age'],
-      );
+    List<Map<String, dynamic>> mapRows = await db.query('dogs');
+    return List.generate(mapRows.length, (position) {
+      var dog = Dog(
+          id: mapRows[position]['id'],
+          name: mapRows[position]['name'],
+          age: mapRows[position]['age']);
+      print(dog.toString());
+      return dog;
     });
   }
 
-  Future<void> updateDog(Dog dog) async {
+  Future<int> updateDog(Dog dog) async {
     final db = await database;
-
-    await db.update(
-      'dogs',
-      dog.toMap(),
-      where: 'id = ?',
-      whereArgs: [dog.id],
-    );
+    int rowPostion = await db
+        .update('dogs', dog.toMap(), where: 'id = ?', whereArgs: [dog.id]);
+    return rowPostion;
   }
 
-  Future<void> deleteDog(int id) async {
+  void deleteDog(int id) async {
     final db = await database;
-
-    await db.delete(
-      'dogs',
-      where: 'id = ?',
-      whereArgs: [id],
-    );
+    await db.delete('dogs', where: 'id = ?', whereArgs: [id]);
   }
 
-  var fido = Dog(
-    id: 0,
-    name: 'Fido',
-    age: 35,
-  );
-
-  await insertDog(fido);
-
-  print(await dogs());
-
-  fido = Dog(
-    id: fido.id,
-    name: fido.name,
-    age: fido.age + 7,
-  );
-  await updateDog(fido);
-
-  print(await dogs());
-
-  await deleteDog(fido.id);
-
-  print(await dogs());
 }
 
 class Dog {
